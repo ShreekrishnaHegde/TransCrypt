ee9ff75d6b850fa45ab0caf83b83a241cf172cd2
Shreekrishna <hegdeshreekrishna469@gmail.com>
2025-10-08 06:47:53 +0530

    msg

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Firebase Auth',
-      home: FileTransferPage(),
+      home: Login(),
     );
   }
 }
